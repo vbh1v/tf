dd216cf608b975b46eaa6b7a1aee106daa0e15be
Onlook <git@onlook.com>
2025-05-04 19:40:48 +0530

    Save before publishing to sco-r1jk6fmzcvpwazd-e.onlook.live

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
+  output: "standalone",
+  distDir: process.env.NODE_ENV === "production" ? ".next-prod" : ".next",
+  typescript: {
+    ignoreBuildErrors: true
+  }
 };
 export default nextConfig;
